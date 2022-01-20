18aef5a9dbf5476e51b30b7254d0b81c9e60b089
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-01-20 15:48:34 -0600

    jquery practice complete

---
+++ js/jquery-practice.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$("td").css("border", "solid 1px black");
+$("li").css("list-style-type", "none");
+$("body").css("font-family", "'Abril Fatface', cursive");
+$("#nav").css("background-color", "lightpink");
+$("#header").css("background-color", "lightyellow");
+$("#section-one").css("background-color", "lightgreen");
+$("#table").css("background-color", "lightblue");
+$(".highlight").css("background-color", "yellow");
+$("img").click(function () {
+    $(this).css("width", "400px");
+})
