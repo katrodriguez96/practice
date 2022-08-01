e16c025a9da554189b8f1eeadf4e069d7f7d3d96
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-08-01 14:21:45 -0500

    completed question i was stuck on

--- codesignal/almostIncreasingSequence.js
+++ codesignal/almostIncreasingSequence.js
@@ -1,24 +1,38 @@
+// function solution(sequence) {
+//     let result = false;
+//     for (let i = 0; i < sequence.length; i++) {
+//         if (result === true) {
+//             break;
+//         }
+//         let testArr = sequence.slice();
+//         testArr.splice(i, 1);
+//         if (testArr.length === 1) {
+//             result = true;
+//         }
+//         for (let j = 0; j < testArr.length - 1; j++) {
+//             if (testArr[j] < testArr[j + 1]) {
+//                 result = true;
+//             } else {
+//                 result = false;
+//                 break;
+//             }
+//         }
+//     }
+//     return result;
+// }
+
 function solution(sequence) {
-    let result = false;
+    let bad = 0;
     for (let i = 0; i < sequence.length; i++) {
-        if (result === true) {
-            break;
-        }
-        let testArr = sequence.slice();
-        testArr.splice(i, 1);
-        if (testArr.length === 1) {
-            result = true;
-        }
-        for (let j = 0; j < testArr.length - 1; j++) {
-            if (testArr[j] < testArr[j + 1]) {
-                result = true;
-            } else {
-                result = false;
-                break;
+        if (sequence[i] <= sequence[i-1]) {
+            bad++;
+            if (bad > 1) {
+                return false;
+            }
+            if (sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1]) {
+                return false;
             }
         }
     }
-    return result;
+    return true;
 }
-
-console.log(solution([1, 1]));
